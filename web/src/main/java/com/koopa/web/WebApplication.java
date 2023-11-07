678369b68e8cc8cf83e5ddedac1f6ac939cfec72
francis <francis.b.tran@gmail.com>
2023-11-06 22:42:14 -0800

    test

--- web/src/main/java/com/koopa/web/WebApplication.java
+++ web/src/main/java/com/koopa/web/WebApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
     public static void main(String[] args) {
+        // test
         SpringApplication.run(WebApplication.class, args);
     }
 }
