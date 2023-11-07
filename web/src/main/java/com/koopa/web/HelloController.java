3fc3419c48f55ceb103e50e02bba87fe65a1cc6e
francis <francis.b.tran@gmail.com>
2023-11-06 23:11:38 -0800

    Hello World!

---
+++ web/src/main/java/com/koopa/web/HelloController.java
@@ -0,0 +1,17 @@
+package com.koopa.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+// Source: https://spring.io/quickstart
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index(
+        @RequestParam(value = "name", defaultValue = "World") String name
+    ) {
+        return String.format("Hello %s!", name);
+    }
+}
--- web/src/main/java/com/koopa/web/WebApplication.java
+++ web/src/main/java/com/koopa/web/WebApplication.java
@@ -2,8 +2,6 @@ package com.koopa.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class WebApplication {
@@ -11,12 +9,5 @@ public class WebApplication {
     public static void main(String[] args) {
         // test
         SpringApplication.run(WebApplication.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String hello(
-        @RequestParam(value = "name", defaultValue = "World") String name
-    ) {
-        return String.format("Hello %s!", name);
     }
 }
