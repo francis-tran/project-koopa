4bcd259173624f03f9188c37378a88db0e54f776
francis <francis.b.tran@gmail.com>
2023-11-09 13:50:09 -0800

    Revert "HelloModule Example"
    This reverts commit 93297a2462a19e14c899bd083b56babf1f88639e.

--- web/src/test/java/com/koopa/web/WebApplicationTests.java
+++ web/src/test/java/com/koopa/web/WebApplicationTests.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebApplicationTests {
 
-    // More context: https://docs.spring.io/spring-boot/docs/1.5.7.RELEASE/reference/html/boot-features-testing.html
     @Test
     void contextLoads() {}
 }
