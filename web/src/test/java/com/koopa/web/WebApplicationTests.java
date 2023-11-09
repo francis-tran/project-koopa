93297a2462a19e14c899bd083b56babf1f88639e
francis <francis.b.tran@gmail.com>
2023-11-09 12:15:32 -0800

    HelloModule Example

--- web/src/test/java/com/koopa/web/WebApplicationTests.java
+++ web/src/test/java/com/koopa/web/WebApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class WebApplicationTests {
 
+    // More context: https://docs.spring.io/spring-boot/docs/1.5.7.RELEASE/reference/html/boot-features-testing.html
     @Test
     void contextLoads() {}
 }
