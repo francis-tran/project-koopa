f05933435743ce6dc05d3f64c7e1ef2ba1a599ae
francis <francis.b.tran@gmail.com>
2023-11-09 15:25:57 -0800

    HelloModule example

--- web/src/main/java/com/koopa/web/HelloModule/HelloService.java
+++ web/src/main/java/com/koopa/web/HelloModule/HelloService.java
@@ -1,3 +1,4 @@
 package com.koopa.web.HelloModule;
 
+// Notice no @Service annotation here because it is an interface
 public interface HelloService {}
--- web/src/main/java/com/koopa/web/HelloModule/HelloServiceImpl.java
+++ web/src/main/java/com/koopa/web/HelloModule/HelloServiceImpl.java
@@ -1,3 +1,6 @@
 package com.koopa.web.HelloModule;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class HelloServiceImpl implements HelloService {}
