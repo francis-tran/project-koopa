f9ad0a3a9b8c335ffa3a8a0f3e61d760081fd736
francis <francis.b.tran@gmail.com>
2023-11-08 18:33:03 -0800

    HelloModule

--- web/src/main/java/com/koopa/web/HelloModule/HelloController.java
+++ web/src/main/java/com/koopa/web/HelloModule/HelloController.java
@@ -1,4 +1,4 @@
-package com.koopa.web;
+package com.koopa.web.HelloModule;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
