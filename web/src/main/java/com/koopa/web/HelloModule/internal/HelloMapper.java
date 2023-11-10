46a21318dbd1a7d1f69701b89a38fabfdd2bfa8d
francis <francis.b.tran@gmail.com>
2023-11-09 20:02:23 -0800

    HelloModule example

--- web/src/main/java/com/koopa/web/HelloModule/internal/HelloMapper.java
+++ web/src/main/java/com/koopa/web/HelloModule/internal/HelloMapper.java
@@ -1,3 +1,4 @@
 package com.koopa.web.HelloModule.internal;
 
+// Each model class can have one or more DTO's. The mapper is responsible for mapping each DTO class to a model class
 public class HelloMapper {}
