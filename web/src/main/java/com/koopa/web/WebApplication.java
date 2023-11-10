c9c5e0ace2b52957ae6c4e634ba17749b65a0a5a
francis <francis.b.tran@gmail.com>
2023-11-09 19:40:22 -0800

    Removed comment in WebApplication.java

--- web/src/main/java/com/koopa/web/WebApplication.java
+++ web/src/main/java/com/koopa/web/WebApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebApplication {
 
     public static void main(String[] args) {
-        // test
         SpringApplication.run(WebApplication.class, args);
     }
 }
