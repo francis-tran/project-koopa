cc597e2556da97bef8c6e92d28ca57257c599566
francis <francis.b.tran@gmail.com>
2023-11-10 12:26:48 -0800

    Added @Service to HelloServiceImpl

--- web/src/main/java/com/koopa/web/HelloModule/internal/HelloServiceImpl.java
+++ web/src/main/java/com/koopa/web/HelloModule/internal/HelloServiceImpl.java
@@ -2,7 +2,9 @@ package com.koopa.web.HelloModule.internal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class HelloServiceImpl implements HelloService {
 
     @Autowired
